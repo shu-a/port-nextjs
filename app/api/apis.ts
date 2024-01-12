c9944181cd7723d56e25dfdfc42717bcbd29534d
shu <master@shu-a.app>
2024-01-12 10:24:16 +0900

    Add baseUrl

--- app/api/apis.ts
+++ app/api/apis.ts
@@ -1,6 +1,4 @@
-import axios from "axios";
-
 export const getTopMenu = async () => {
-  const res = await axios("http://localhost:3000/api/topmenu");
-  return res.data.result.content;
+  const res = await fetch(`${process.env.BASE_URL}/api/topmenu`);
+  return res.json();
 };
--- app/ui/main-board.tsx
+++ app/ui/main-board.tsx
@@ -4,51 +4,54 @@ import { BoardHeaderSkeleton } from "./skeletons";
 import BoardHeader from "./board/board-header";
 import MainBoardContent from "./main-board-content";
 
+interface Content {
+  title: string;
+  content: [
+    {
+      seq: number;
+      user: string;
+      view: number;
+      date: string;
+      title: string;
+      vote: number;
+      commentCount: 0;
+    }
+  ];
+}
+
+interface Board {
+  result: [Content];
+}
 const MainBoard = async () => {
   const boardlist = await getTopMenu();
 
   return (
     <>
-      {boardlist.map(
-        (board: {
-          title: string;
-          content: [
-            {
-              seq: number;
-              user: string;
-              view: number;
-              date: string;
-              title: string;
-              vote: number;
-              commentCount: 0;
-            }
-          ];
-        }) => (
+      {boardlist.result.content.map((board: Content) => (
+        <div
+          key={board.title}
+          className="flex flex-row items-center justify-center w-full min-h-[700px]"
+        >
           <div
             key={board.title}
-            className="flex flex-row items-center justify-center w-full min-h-[700px]"
+            className="flex flex-col p-3 w-full h-full itmes"
           >
-            <div
-              key={board.title}
-              className="flex flex-col p-3 w-full h-full itmes"
-            >
-              <div>
-                <Suspense fallback={<BoardHeaderSkeleton />}>
-                  <BoardHeader title={board.title} />
-                </Suspense>
-              </div>
-              <div className="flex items-center justify-center h-[400px] mt-3">
-                <div className="flex flex-col w-full h-full">
-                  {board?.content.length > 0 &&
-                    board.content.map((content) => (
-                      <MainBoardContent key={content.seq} content={content} />
-                    ))}
-                </div>
+            <div>
+              <Suspense fallback={<BoardHeaderSkeleton />}>
+                <BoardHeader title={board.title} />
+              </Suspense>
+            </div>
+            <div className="flex items-center justify-center h-[400px] mt-3">
+              <div className="flex flex-col w-full h-full">
+                {board?.content.length > 0 &&
+                  board.content.map((content) => (
+                    <MainBoardContent key={content.seq} content={content} />
+                  ))}
               </div>
             </div>
           </div>
-        )
-      )}
+        </div>
+      ))}
     </>
   );
 };
