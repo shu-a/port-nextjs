bfabb02423b3f4fdeb8f29a3e7831b1d3c85e6f2
shu <master@shu-a.app>
2024-01-12 10:26:34 +0900

    Update apis.ts

--- app/api/apis.ts
+++ app/api/apis.ts
@@ -1,4 +1,11 @@
+import axios from "axios";
+
 export const getTopMenu = async () => {
-  const res = await fetch(`${process.env.BASE_URL}/api/topmenu`);
-  return res.json();
+  const res = await axios(`${process.env.BASE_URL}/api/topmenu`);
+  return res.data;
 };
+
+// export const getTopMenu = async () => {
+//   const res = await fetch(`${process.env.BASE_URL}/api/topmenu`);
+//   return res.json();
+// };
