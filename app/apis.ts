07510d71b855b9735561753d5cba9c4e4282059b
shu <master@shu-a.app>
2024-01-12 14:37:39 +0900

    Update apis.ts

--- app/apis.ts
+++ app/apis.ts
@@ -1,6 +1,6 @@
 export const getTopMenu = async () => {
   try {
-    const res = await fetch(`${process.env.PROTOCOL}${process.env.VERCEL_URL}/api/topmenu`);
+    const res = await fetch(`${process.env.PROTOCOL}//${process.env.VERCEL_URL}/api/topmenu`);
     return res.json();
   } catch (error) {
     console.error("getTopMenu:", error);
