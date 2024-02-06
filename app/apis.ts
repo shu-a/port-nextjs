e2b639023ca9acbf599c448dc6e35fb530daa01f
shu <master@shu-a.app>
2024-02-06 23:58:41 +0900

    Update apis.ts
    api 주소 변경

--- app/apis.ts
+++ app/apis.ts
@@ -4,11 +4,11 @@ headers.set('x-api-key', process.env['X_API_KEY'] as string);
 
 export const getTopMenu = async () => {
   try {
-    const res = await fetch(`${process.env.PROTOCOL}//${process.env.VERCEL_URL}/api/topmenu`, {
+    const res = await fetch(`${process.env.API_URL}/api/topmenu`, {
+    // const res = await fetch(`${process.env.PROTOCOL}//${process.env.VERCEL_URL}/api/topmenu`, {
       method: 'GET',
       headers,
     });
-    // `${process.env.PROTOCOL}//${process.env.VERCEL_URL}/api/topmenu`,
     return res.json();
   } catch (error) {
     console.error('getTopMenu:', error);
